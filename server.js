552bdc1b51889dacc0d057b12e968a4e92c1fae6
KelahKelah <kelechiekebor@gmail.com>
2020-07-10 13:08:34 +0100

    express server added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "build")))
+
+app.all("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/build/index.html"));
+})
+
+http.createServer(app).listen()
+
+app.listen(PORT).on('listening', (details) => {
+    console.log("server listening on: ", PORT);
+})
