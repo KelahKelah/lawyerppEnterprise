a05b42abd304a75288b97585d9f0984c15e93b08
Iheanyi25 <ihekweabai@gmail.com>
2020-07-14 16:36:23 +0100

    fetched from the api

--- src/pages/court/viewOrganiztions.js
+++ src/pages/court/viewOrganiztions.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from 'axios'
 
-function ViewOrganiztions() {
-    return (
-        <div>
-            <h1>Created Organiztions</h1>
-        </div>
-    )
-}
+const ViewOrganiztions = () => {
+  useEffect(() => {
+    axios
+      .get(
+        "https://lawyerppenterprise.herokuapp.com/api/corporg/user_orgs/:15/:1"
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {});
+  }, []);
 
-export default ViewOrganiztions
+  return (
+    <div>
+      <h1>Created Organiztions</h1>
+    </div>
+  );
+};
+
+export default ViewOrganiztions;
