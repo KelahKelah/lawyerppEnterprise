58afe0680210a2b1cfa9afdbebf4c8086f8bcd6c
Iheanyi25 <ihekweabai@gmail.com>
2020-07-14 09:49:11 +0100

    Created a success page and added it to the create court component

---
+++ src/components/success/success.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./success.css";
+import { Link } from "react-router-dom";
+
+const Success = (props) => {
+  const mystyle = {
+    borderRadius: "200px",
+    height: "200px",
+    width: "200px",
+    background: "#F8FAF5",
+    margin: "0 auto",
+  };
+  if (props.type === "filedProcess")
+    return (
+      <div className="body">
+        <div className="card">
+          <div style={mystyle}>
+            <i className="checkmark">✓</i>
+          </div>
+          <h1>Success</h1>
+          <p>
+            {props.message};
+            <br /> Click <Link to={`${props.link}`}>here</Link> to view
+            {props.direction} or click <Link to="/home">here</Link> to go back
+            home!
+          </p>
+        </div>
+      </div>
+    );
+  if (props.type === "createdCourt")
+    return (
+      <div className="body">
+        <div className="card">
+          <div style={mystyle}>
+            <i className="checkmark">✓</i>
+          </div>
+          <h1>Success</h1>
+          <p>
+            {props.message};
+            <br /> Click <Link to={`${props.link}`}>here</Link> to view
+            {props.direction} or click <Link to="/home">here</Link> to go back
+            home!
+          </p>
+        </div>
+      </div>
+    );
+};
+
+export default Success;
