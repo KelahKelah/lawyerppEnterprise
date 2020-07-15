8ed186d8e84c7c1871f11bd6692377b9d1ffb81e
Iheanyi25 <ihekweabai@gmail.com>
2020-07-15 10:29:39 +0100

    Started the view organization page

--- src/pages/court/viewOrganiztions.js
+++ src/pages/court/viewOrganiztions.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const ViewOrganiztions = () => {
   useEffect(() => {
@@ -14,8 +14,33 @@ const ViewOrganiztions = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Created Organiztions</h1>
+    <div className="container mt-4">
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name of Organization</th>
+            <th scope="col"></th>
+            <th scope="col">Opposing Party</th>
+            <th scope="col">Opposing Lawyer</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            className="c-pointer court-tr"
+            data-target="#moreInfo"
+            data-toggle="modal"
+          >
+            <th scope="row">1</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
