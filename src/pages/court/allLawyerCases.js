0249aa6089b467087dec9d9c6e4d05af98bdcb8e
Iheanyi25 <ihekweabai@gmail.com>
2020-07-21 11:50:23 +0100

    Added a view cert button to this components

--- src/pages/court/allLawyerCases.js
+++ src/pages/court/allLawyerCases.js
@@ -95,7 +95,7 @@ const LawyerCases = (props) => {
             <th scope="col">Mode of Commencement</th>
             <th scope="col">Opposing Party</th>
             <th scope="col">Opposing Lawyer</th>
-            <th></th>
+            <th scope="col">Process Document</th>
           </tr>
         </thead>
         <tbody>
@@ -133,11 +133,11 @@ const LawyerCases = (props) => {
                   <td>
                     {singleCase.processImageUrl !== "" ? (
                       <a
-                        className="btn btn-primary text-white"
+                        className="btn btn-primary btn-block text-white"
                         href={`${singleCase.processImageUrl}`}
                         target="_blank"
                       >
-                        file
+                        View document
                       </a>
                     ) : (
                       "no file"
@@ -527,6 +527,19 @@ const LawyerCases = (props) => {
                                     .opposing_office_address}
                               </small>
                               <br />
+                              <br />
+                              <b>Process Document: </b>
+                              {caseDetail.processImageUrl !== "" ? (
+                                <a
+                                  className="btn btn-primary btn-sm text-white"
+                                  href={`${caseDetail.processImageUrl}`}
+                                  target="_blank"
+                                >
+                                  View document
+                                </a>
+                              ) : (
+                                "no file"
+                              )}
                             </div>
                           </div>
                         </div>
